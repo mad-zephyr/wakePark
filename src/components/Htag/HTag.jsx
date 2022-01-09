f9e51efd96a5eecc9da128040d1bf7cb58cdd974
vasilii yatsyuk <72764693+mad-zephyr@users.noreply.github.com>
2022-01-10 01:28:36 +0200

    Develop main screen and styles

--- src/components/Htag/HTag.jsx
+++ src/components/Htag/HTag.jsx
@@ -9,16 +9,23 @@ const Htag = (props) => {
 
   switch (tag) {
   case 'h1':
-    return <h1 className={styles.h1} >{text}</h1>
+    return <h1 className={cn(styles.h, styles.h1, {
+      [styles.h3__dark]: type === 'dark',
+      [styles.h3__light]: type === 'light',
+    })} >{text}</h1>
+
   case 'h2':
     return <h2 className={cn(styles.h, styles.h2)} >{text}</h2>
+
   case 'h3':
     return <h3 className={cn(styles.h, styles.h3, {
       [styles.h3__dark]: type === 'dark',
       [styles.h3__light]: type === 'light',
     })} >{text}</h3>
+
   case 'h4':
     return <h4 className={cn(styles.h, styles.h4)} >{text}</h4>
+    
   default:
     return <> </>
   }
--- src/page-component/hero-screen/HeroScreen.jsx
+++ src/page-component/hero-screen/HeroScreen.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
-// import PropTypes from 'prop-types' 
-// import cn from 'classnames'
-import styles from './HeroScreen.sass'
-import './HeroScreen.sass'
+import Button from '../../components/button/button'
+import Htag from '../../components/Htag/HTag'
+import styles from './HeroScreen.module.sass'
+import P from '../../components/p/p'
 
+const btnName='Посмотреть видео презентацию нашего вейкпарка'
+const desqText = 'Мы всегда рядом с теми, кто объединяется ради любви к спорту, здоровому образу жизни и активному отдыху всей семьей.'
 
 const HeroScreen = () => {
 
   return(
-    <section className='hero'>
- 
+    <section className={styles.hero}>
+
+      <div className="container">
+        <div className={styles.hero__wrapper}>
+          <Htag tag='h1' color='white' type='light' text={'WAKE days\nARE GOOD DAYS'}/>
+          <P color='yellow' text='#WAKEPARKMD'/>
+
+          <div className={styles.descriptor}>
+            <P textSize='m' color='white' text={desqText}/>
+          </div>
+
+          <Button type='left' iconName='play' color='yellow' textSize='s' text={btnName}/>
+        </div>
+        
+        
+        <div className={styles.overlay}/>
+      </div>
+      <div className={styles.hero__img}/>
       <div className={styles.background}/>
     </section>
   )
